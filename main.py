64965a00a875f2163ada8e256aae228633b28f30
Dreamer <skilet.e@yandex.ru>
2023-01-06 14:54:57 +0300

    In proggres

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import random
 class Player:
 
     hunger = 50
@@ -6,10 +7,11 @@ class Player:
     leisure = 50
     money = 0
     age = 5
-    day = 1
+    day = 360
     day_in_scool = 0
     chcoll_class = 1
     place_of_work = "Без работы"
+    post = ""
     education = "Без образования"
     rep = 0
     car = "Без машины"
@@ -21,13 +23,69 @@ class Player:
 
     def work(self):
         if self.place_of_work == "Без работы":
-            if self.age == 14:
+            if self.age >= 14 and self.age < 18:
                 print("С 14 лет вам доступные работы:\nРазносить почту\nВыгул собак\nПромоутер")
-            elif self.age == 18:
-                print("С 18 лет вам доступны работы:\nПолиция\nВрач\nУчитель\nПожарный\nАртист\nДепутат")
-        else:
-            self.money += 1000
+                choice = input("Выберете профессию: ")
+                if choice == "Разносить почту":
+                    print("Обычный день, почтальона + 500 рублей")
+                    self.money += 500
+                    self.sleep -= 20
+                elif choice == "Выгул собак":
+                    print("Взял поводок, вывел собаку , завел домой и так целый день + 700 рублей")
+                    self.money += 700
+                    self.sleep -= 30
+                elif choice == "Промоутер":
+                    print("Стоял весь день у торгового центра + 400 рублей")
+                    self.money += 400
+                    self.sleep -= 10
+                else:
+                    print("не корректный ввод")
+                    time.sleep(1)
+                    self.work()
 
+            elif self.age >= 18:
+                print("С 18 лет вам доступны работы:\nПолиция\nВрач\nУчитель\nПожарный\nАртист\nДепутат")
+                choice = input("Выберете профессию: ")
+                if choice == "Полиция":
+                    self.place_of_work = "Полиция"
+                    self.post = "Рядовой"
+                elif choice == "Врач":
+                    self.place_of_work = "Больница"
+                    self.post = 'Младший мед персонал'
+                elif choice == "Учитель":
+                    self.place_of_work = "Школа"
+                    self.post = "Учитель начальных классов"
+                elif choice == "Пожарный":
+                    self.place_of_work = "Пожарная часть"
+                    self.post = "Пожарный"
+                elif choice == "Артист":
+                    self.place_of_work = "Улица"
+                    self.post = "Музыкант в переходе"
+                elif choice == "Депутат":
+                    if self.chcoll_class > 9 and self.age > 21:
+                        self.place_of_work = "Местный отдел партии"
+                        self.post = 'Помощьник депутата'
+                    else:
+                        print("Ты не можешь работать депутатом! не достаточно опыта")
+                else:
+                    print("Не корректный ввод")
+                    time.sleep(1)
+                    self.work()
+
+            else:
+                print("Ты еще слишком маленький")
+        elif self.place_of_work == "Полиция":
+            self.money += 1500
+        elif self.place_of_work == "Врач":
+            self.money += 1000
+        elif self.place_of_work == "Учитель":
+            self.money += 700
+        elif self.place_of_work == "Пожарный":
+            self.money += 1300
+        elif self.place_of_work == "Артист":
+            self.money += random.randint(1,4000)
+        elif self.place_of_work == "Депутат":
+            self.money += 5000
     def sleeping(self):
         pass
 
@@ -46,7 +104,7 @@ class Player:
     def print_info(self):
         print("\n"*50)
         print("Сытость: {}\nБодрость: {}\nДосуг: {}\nДеньги : {} рублей\nВозраст: {} лет"
-              "\nДней прошло: {}день(ей)\nМесто работы: {}\nОброзование: {}\nРепутация: {}\nМашина: {}"
+              "\nДней прошло: {} день(ей)\nМесто работы: {}\nОброзование: {}\nРепутация: {}\nМашина: {}"
               "\nКвартира: {}".format(self.hunger,
                                       self.sleep,self.leisure,self.money,self.age,
                                       self.day,self.place_of_work,self.education,self.rep,self.car,self.home))
@@ -55,9 +113,57 @@ class Player:
         if self.day == 365:
             self.day = 1
             self.age += 1
-        self.sleep -= 20
+            print("У тебя день рождения,пришли гости и принесли подарки")
+            self.sleep += 100
+            self.money += 5000
+            self.hunger += 100
+            self.leisure += 100
+
+        self.sleep -= 10
         self.hunger -= 10
         self.leisure -= 10
+    def step_chool(self):
+        self.day_in_scool += 1
+
+        if self.chcoll_class == 9 and self.age > 16:
+            print("ты закончил 9 классов поздравляю")
+            self.education = "Среднее не полное"
+
+        elif self.day_in_scool == 15 and self.age == 8:
+            self.day_in_scool = 1
+            self.chcoll_class = 2
+
+        elif self.day_in_scool == 15 and self.age == 9:
+            self.day_in_scool = 1
+            self.chcoll_class = 3
+
+        elif self.day_in_scool == 15 and self.age == 10:
+            self.day_in_scool = 1
+            self.chcoll_class = 4
+
+        elif self.day_in_scool == 15 and self.age == 11:
+            self.day_in_scool = 1
+            self.chcoll_class = 4
+
+        elif self.day_in_scool == 15 and self.age == 12:
+            self.day_in_scool = 1
+            self.chcoll_class = 5
+
+        elif self.day_in_scool == 15 and self.age == 13:
+            self.day_in_scool = 1
+            self.chcoll_class = 6
+
+        elif self.day_in_scool == 15 and self.age == 14:
+            self.day_in_scool = 1
+            self.chcoll_class = 7
+
+        elif self.day_in_scool == 15 and self.age == 15:
+            self.day_in_scool = 1
+            self.chcoll_class = 8
+
+        elif self.day_in_scool == 15 and self.age == 16:
+            self.day_in_scool = 1
+            self.chcoll_class = 9
 
 def choice_pl(name):
     time.sleep(1)
@@ -79,49 +185,60 @@ def choice_pl(name):
             else:
                 print("Денег не хватило иди на работу")
         name.step_day()
+        time.sleep(1)
         name.print_info()
         choice_pl(name)
     elif choice == 2:
         print("ТЫ пошел спать")
         name.sleep += 50
         name.step_day()
+        time.sleep(1)
         name.print_info()
         choice_pl(name)
+
     elif choice == 3:
         print("Ты пошел на прогулку с друзьями")
-        name.leisure += 30
+        name.leisure += 60
         name.step_day()
+        time.sleep(1)
         name.print_info()
         choice_pl(name)
     elif choice == 4:
-        if name.age > 6:
+        if name.age >= 7:
             if name.day_in_scool == 1 and name.chcoll_class == 1:
                 print("Теперь ты можешь пойти учиться в первый класс!")
-                
+                name.step_chool()
+            else:
+                name.step_chool()
+
 
         name.step_day()
+        time.sleep(1)
         name.print_info()
         choice_pl(name)
     elif choice == 5:
+        name.work()
         name.step_day()
+        time.sleep(1)
         name.print_info()
         choice_pl(name)
     elif choice == 6:
         name.step_day()
+        time.sleep(1)
         name.print_info()
         choice_pl(name)
     else:
         print("Не корректный ввод")
         choice_pl(name)
 
-def mainMenu():
+def mainMenu(name,country):
     print("Добро пожаловать в симулятор жизни!")
-    name = input("Введите имя вашего персонажа: ")
-    country = input("Введите старну рождения вашего персонажа: ")
     name = Player(name,country)
     name.print_info()
     time.sleep(3)
     print("\n"*15)
     choice_pl(name)
 
-mainMenu()
+name = input("Введите имя вашего персонажа: ")
+country = input("Введите старну рождения вашего персонажа: ")
+mainMenu(name,country)
